a80174a03fac68a6360c11cdd265d54b70351039
Wang Le <wangle@e-accent.com>
2011-05-26 15:16:32 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wicked::Application.config.secret_token = 'be47200e64c902b1faa3a5ac3c1752dca9117e2df203bb2bc40da0964eec237e0c3c9bb6b3f82ba0886ceec80a3dcd22adf442331ce1144cfc904542372d72fb'
